f1ae90d4ed4af9eebca396c5e2878f43c822c8aa
Dalmas <akumuodhiambo@gmail.com>
2024-09-13 16:43:47 +0300

    All Models Created and Registered

--- tree_tracker/MyTree/admin.py
+++ tree_tracker/MyTree/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from MyTree.models import *
 
-# Register your models here.
+@admin.register(Tree)
+class TreeAdmin(admin.ModelAdmin):
+    list_display = ('species', 'planted_on', 'location', 'user')
+    search_fields = ('species', 'location')
+
+@admin.register(PlantingEvent)
+class PlantingEventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'event_date', 'location', 'created_by')
+    search_fields = ('name', 'location')
+
+@admin.register(TreeGrowthRecord)
+class TreeGrowthRecordAdmin(admin.ModelAdmin):
+    list_display = ('tree', 'record_date', 'height', 'health_status')
+    search_fields = ('tree__species',)
+
+@admin.register(ImpactReport)
+class ImpactReportAdmin(admin.ModelAdmin):
+    list_display = ('report_date', 'total_trees_planted', 'total_carbon_sequestered')
+    search_fields = ('report_date',)
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bio')
+    search_fields = ('user__username',)
